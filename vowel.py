c94f59a6fd84d07153b99d809f11b1cf7cd2bfc4
manuel dellagala <manuel.dellagala@gmail.com>
2021-11-02 22:43:24 +0100

    ok

--- vowel.py
+++ vowel.py
@@ -1,10 +1,10 @@
 def isVowel(letter):
     if letter in "aeiou":
-        return False
-    else:
         return True
+    else:
+        return False
 
 
 
-x = input("inserisci lettera")
-print (isVowel(x))
+x = input("inserisci lettera: ")
+print ("è una vocale" if isVowel(x) else "non è una vocale")
